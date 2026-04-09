88c8096e6881ddaf75db07678a6563b4794079ac
ahmad-bhai <nasiriq768@gmail.com>
2026-04-10 02:01:05 +0500

    Add files via upload

---
+++ postback.js
@@ -0,0 +1,30 @@
+export default async function handler(req, res) {
+    const { uid, status } = req.query;
+    const BOT_ID = "8320428359";
+    const API_KEY = "AAET0DP7fhG5VLXM-8ge7jzU8ck9oYbulH0";
+
+    if (!uid) {
+        return res.status(400).send("No ID provided");
+    }
+
+    try {
+        const bjs_url = `https://api.bots.business/v1/bots/${BOT_ID}/properties`;
+        
+        await fetch(bjs_url, {
+            method: 'POST',
+            headers: { 
+                'Content-Type': 'application/json',
+                'api-key': API_KEY 
+            },
+            body: JSON.stringify({
+                name: "qx_" + uid,
+                value: status || "registered",
+                type: "string"
+            })
+        });
+
+        return res.status(200).send("Success! Data sent to Bot.");
+    } catch (error) {
+        return res.status(500).send(error.message);
+    }
+}
